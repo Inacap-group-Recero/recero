2143dcc0ca5683345654105f3b1c9a30459625c0
moya2022 <nalejandromoya@gmail.com>
2021-10-20 01:56:22 -0300

    added login test
    v1.0

---
+++ index.php
@@ -0,0 +1,31 @@
+<html>
+<head>
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+<title>PHP Test</title>
+</head>
+<body>
+
+<div class="container">
+<div class="row">
+<div class="col">
+</div>
+<div class="col-6">
+<nav class="navbar navbar-light bg-light">
+<div class="container">
+<a class="navbar-brand" href="#">
+<img src="assets/img/login/megaman.jpg" alt="" width="54" height="54">
+<br>
+<h5> Bienvenidos Colegas del mundo</h5>
+<br>
+
+</a>
+</div>
+</nav>
+</div>
+<div class="col">
+</div>
+</div>
+</div>
+</body>
+</html>
